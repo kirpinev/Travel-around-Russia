2d23770422246b4d3ef589c327cfa335a614f092
Igor Kirpinev <kirpinev@yahoo.com>
2021-01-07 13:49:40 +0300

    Добавил метатеги

--- components/Page/index.tsx
+++ components/Page/index.tsx
@@ -1,7 +1,18 @@
+import Head from "next/head";
 import { FC } from "react";
 
 import { IProps } from "./types";
 
 export const Page: FC<IProps> = ({ children }): JSX.Element => (
-  <div className="page">{children}</div>
+  <div className="page">
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <meta name="author" content="Игорь Кирпинев" />
+      <meta name="description" content="Интересные места России" />
+      <title>Интересные места России</title>
+    </Head>
+    {children}
+  </div>
 );
